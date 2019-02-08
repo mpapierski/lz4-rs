c9c02d3f80234b3423ad325f15e964d6fa5f1656
Michał Papierski <michal@papierski.net>
2019-02-08 17:06:28 +0100

    Force endianness

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -11,16 +11,21 @@ fn main() {
         .file("liblz4/lib/xxhash.c")
         // We always compile the C with optimization, because otherwise it is 20x slower.
         .opt_level(3);
-    match env::var("TARGET").unwrap().as_str()
-    {
-      "i686-pc-windows-gnu" => {
-        compiler
-            .flag("-fno-tree-vectorize");
-      },
-      _ => {}
+    match env::var("TARGET").unwrap().as_str() {
+        "i686-pc-windows-gnu" => {
+            compiler.flag("-fno-tree-vectorize");
+        }
+        _ => {}
     }
     compiler.define("LZ4_FORCE_MEMORY_ACCESS", "0");
     compiler.define("LZ4_DEBUG", "2");
     compiler.define("LZ4F_HEAPMODE", "1");
+    if let Ok(target_endian) = env::var("CARGO_CFG_TARGET_ENDIAN") {
+        if target_endian == "big" {
+            compiler.define("XXH_CPU_LITTLE_ENDIAN", Some("0"));
+        } else {
+            compiler.define("XXH_CPU_LITTLE_ENDIAN", Some("1"));
+        }
+    }
     compiler.compile("liblz4.a");
 }
