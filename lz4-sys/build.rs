810696d277f4f686e59cf2dacd697a3aaa377848
Michał Papierski <michal@papierski.net>
2019-02-08 16:10:19 +0100

    Set memory access properly

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -19,6 +19,6 @@ fn main() {
       },
       _ => {}
     }
-    compiler.flag("-DLZ4_FORCE_MEMORY_ACCESS=0");
+    compiler.define("LZ4_FORCE_MEMORY_ACCESS", "0");
     compiler.compile("liblz4.a");
 }
